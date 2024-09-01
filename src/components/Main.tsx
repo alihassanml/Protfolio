fbf9b32be68ed717d6a6bf670183a7828bb164db
Ali Hassan <165924119+alihassanml@users.noreply.github.com>
2024-09-01 19:40:09 +0500

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -18,7 +18,7 @@ function Main() {
         <h1 className="main-heading-01">Hi! I'm <span style={{color:'#2dba4e'}}> Ali Hassan</span></h1>
         <h2 className="main-heading-02">I am <span style={{color:'#2dba4e'}}> Data Scientist </span></h2>
         <p className="main-heading-03">Hi, I am a Machine Learning and Deep Learning specialist, innovating in Generative AI and LangChain. As a Full Stack Developer, I have expertise in Scikit learn,Tensorflow, ANN, CNN React, Django, and FastAPI.</p>
-        <a href="https://drive.google.com/uc?export=download&id=1_rSTZpBRD8DwFFwmRtYSf8CJIjhGTUpd"  style={{textDecoration:"None" , color:'white',cursor:'pointer'}} download><button className="main-button-1">Download CV</button></a>
+        <a href="https://drive.google.com/uc?export=download&id=1fhvpH9UWZWVMI3kpYWQknNDFjEBEhW4z"  style={{textDecoration:"None" , color:'white',cursor:'pointer'}} download><button className="main-button-1">Download CV</button></a>
       </div>
       <div className="main-right col-6">
         <div className="main-right-image"></div>
